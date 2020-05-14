ef9feffd377858743efd9ac0d47c85773a4b22c1
Leonardo Rocha <leopdor@gmail.com>
2020-05-13 23:11:27 -0300

    Atualizações PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationStart, NavigationEnd, NavigationError, Router, Event } from '@angular/router';
 import { AuthorizationService } from './service/auth.service';
+import { SwUpdate, SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,8 @@ import { AuthorizationService } from './service/auth.service';
 export class AppComponent implements OnInit{
   title = 'MeuDogLindo';
   estaLogado = false;
-  constructor(private router: Router, private authService: AuthorizationService) {
+  constructor(private router: Router, private authService: AuthorizationService,
+              private swUpdate: SwUpdate,private swPush: SwPush) {
     this.router.events.subscribe((event: Event) => {
         if (event instanceof NavigationStart) {
         }
@@ -23,5 +25,15 @@ export class AppComponent implements OnInit{
         }
     });
   }
-  ngOnInit() { }
+  ngOnInit() {
+    this.reloadCache();
+   }
+
+  reloadCache() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe( () => {
+          window.location.reload();
+      });
+    }
+  }
 }
