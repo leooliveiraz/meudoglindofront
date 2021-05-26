ee876e2d0331b0b7b2c4bfd883c7266056c41485
Léo Rocha <leopdor@gmail.com>
2021-05-26 10:01:15 -0300

    Correção na lista de usuários e app link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   API_URL: 'https://meudoglindo.herokuapp.com/',
-  APP_URL: 'https://leooliveiraz.github.io/meudoglindofront/',
+  APP_URL: 'https://leooliveiraz.github.io/meupetlindo/',
   GOOGLE_ID: '901875702030-dbvtct82nqvhb393uj118mf8g8g8v66s.apps.googleusercontent.com'
 };
